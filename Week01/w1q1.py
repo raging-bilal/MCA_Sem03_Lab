894255cd426de49e390d865f8df5782f12f57059
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-21 23:46:58 +0530

    Update w1q1.py

--- Week01/w1q1.py
+++ Week01/w1q1.py
@@ -1,1 +1,15 @@
+# Write a program to find product of two user supplied integers and if the productis equal to or lower than 5000 then return sum of the two numbers.
 
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+product= num1 * num2
+
+print(product)
+
+if product <=5000:
+    print(num1 + num2)
+
+else:
+    print(product)
