a487325bd455fd7cded84992579b44ad7ca334f3
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-21 23:47:34 +0530

    Update w1q3.py

--- Week01/w1q3.py
+++ Week01/w1q3.py
@@ -1,10 +1,10 @@
 # Write a program to Iterate the supplied list of 20 numbers by the user and print only those numbers which are divisible by 5.
 
-limit=5
+limit=20
 lst=[]
 i=1
 while i<=limit:
-    num=int(input("Enter 5 values in the list: "))
+    num=int(input("Enter 20 values in the list: "))
     lst.append(num)
     i+=1
 
