60ca584f7ae590702baf531729ab13b58b91edab
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-22 21:33:18 +0530

    Update w2q1.py

--- Week02/w2q1.py
+++ Week02/w2q1.py
@@ -1,1 +1,13 @@
+# Write a Program to extract each digit from an integer in the reverse order.
 
+
+def reverse_digits(num):
+    while num > 0:
+        digit = num % 10  
+        print(digit, end=' ')  
+        num = num // 10  
+
+
+
+num = int(input("Enter an integer: "))
+reverse_digits(num)
