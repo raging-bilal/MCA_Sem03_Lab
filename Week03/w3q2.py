e1355b8ad98d3e803c6d182396736752930f8ba6
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-23 23:27:10 +0530

    Add files via upload

---
+++ Week03/w3q2.py
@@ -0,0 +1,22 @@
+# 2. Write a program to print the following start pattern using the for loop:
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+
+
+
+n = 5
+
+for i in range(1, n + 1):
+    print("* " * i)
+    
+for i in range(n - 1, 0, -1):
+    print("* " * i)
