e97b798010347dc8856a542c4287e721febd35c2
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-23 23:26:27 +0530

    Update w3q1.py

--- Week03/w3q1.py
+++ Week03/w3q1.py
@@ -1,1 +1,23 @@
+# 1. Write a program to print the following pattern using the for loop:
+
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1
+
+
+for i in range(5, 0, -1):
+
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+        
+    print()
+
+
+
+
+
+
+
 
