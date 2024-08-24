6624f4b2aa987363f7cc4ac326a2d32ab010c2ff
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-24 23:45:10 +0530

    Update w4q1.py

--- Week04/w4q1.py
+++ Week04/w4q1.py
@@ -1,1 +1,22 @@
+# 1. Write a program to create function cal_sum_sub() such that it can accept two variables and calculate addition and subtraction.Also, it must return both addition and subtraction in a single return call.
+
+
+def cal_sum_sub(a, b):
+    addition = a + b
+    subtraction = a - b
+    return addition, subtraction
+
+
+result = cal_sum_sub(10, 5)
+print("Addition:", result[0])
+print("Subtraction:", result[1])
+
+
+
+
+
+
+
+
+
 
