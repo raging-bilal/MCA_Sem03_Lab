8a5eeb27c7a2a59becb89d914bdf479520a61933
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-25 23:33:45 +0530

    Update w5q1.py

--- Week05/w5q1.py
+++ Week05/w5q1.py
@@ -1,1 +1,27 @@
+# 1. Write a program to generate 6 digit random secure OTP.
+
+import secrets
+
+def generate_otp():
+    otp = secrets.randbelow(10**6)
+    return f'{otp:06}'  
+
+otp = generate_otp()
+print("Generated OTP:", otp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
