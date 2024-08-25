0fbd41df2fee8566fcf025aa5677e45a102b97ac
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-25 23:34:39 +0530

    Add files via upload

---
+++ Week05/w5q2.py
@@ -0,0 +1,12 @@
+# 2. Write a program to pick a random character from a given String supplied by the user.
+
+import random
+
+def pick_random_char(user_string):
+    if not user_string:
+        return None  
+    return random.choice(user_string)
+
+user_string = input("Enter a string: ")
+random_char = pick_random_char(user_string)
+print("Randomly selected character:", random_char)
