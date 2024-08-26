ce6b294f7d78af310578829957805ed0d4b14e27
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-26 18:27:30 +0530

    Update w6q1.py

--- Week06/w6q1.py
+++ Week06/w6q1.py
@@ -1,1 +1,25 @@
+# 1. Write a Python program that inputs two tuples and creates a third that contains all elements of the first followed by all elements of the second. (You may use other data types such as lists etc. to make your program work)
+
+
+
+def concatenate_tuples(tuple1, tuple2):
+    
+    concatenated_tuple = tuple(list(tuple1) + list(tuple2))
+    return concatenated_tuple
+
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+result = concatenate_tuples(tuple1, tuple2)
+print("Concatenated Tuple:", result)
+
+
+
+
+
+
+
+
+
+
 
