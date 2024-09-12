57be222097c95f29c1b9191f8cfcffe26cfa7ca2
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-12 11:31:32 +0530

    Update w3q1.py

--- Week03/w3q1.py
+++ Week03/w3q1.py
@@ -7,13 +7,11 @@
 # 1
 
 
-for i in range(5, 0, -1):
-
-    for j in range(i, 0, -1):
-        print(j, end=" ")
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
         
+        print(j, end=" ")
     print()
-
 
 
 
