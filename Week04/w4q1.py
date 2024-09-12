db73d3084f6f74b17aa33a9f399e11d6eee8d460
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-12 12:53:13 +0530

    Update w4q1.py

--- Week04/w4q1.py
+++ Week04/w4q1.py
@@ -1,15 +1,23 @@
 # 1. Write a program to create function cal_sum_sub() such that it can accept two variables and calculate addition and subtraction.Also, it must return both addition and subtraction in a single return call.
 
 
-def cal_sum_sub(a, b):
-    addition = a + b
-    subtraction = a - b
-    return addition, subtraction
+def cal_sum_sub_mul_div(a,b):
+    
+    sum=a+b
+    sub=a-b
+    mul=a*b
+    div=a//b
 
+    return sum,sub,mul,div
 
-result = cal_sum_sub(10, 5)
-print("Addition:", result[0])
-print("Subtraction:", result[1])
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+sum,sub,mul,div= cal_sum_sub_mul_div(a,b)
+
+print("The addition of {0} and {1} is {2}.".format(a,b,sum))
+print("The difference of {0} and {1} is {2}.".format(a,b,sub))
+print("The product of {0} and {1} is {2}.".format(a,b,mul))
+print("The quotient of {0} and {1} is {2}.".format(a,b,div))
 
 
 
