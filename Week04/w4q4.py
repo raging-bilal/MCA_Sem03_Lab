54355e764a3011e943f6ae7c9cc7c53f07e75791
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-12 12:56:10 +0530

    Update w4q4.py

--- Week04/w4q4.py
+++ Week04/w4q4.py
@@ -1,11 +1,11 @@
 # 4. Given a two Python list. Write a program to iterate both lists simultaneously and display items from list 1 in original order and items from list 2 in reverse order.
 
-def iterate_lists(lst1, lst2):
-    for x, y in zip(lst1, lst2[::-1]):
-        print(f'List 1 item: {x}, List 2 item: {y}')
+a = [1, 2, 3, 4, 5]
+b = [10, 20, 30, 40, 50]
 
 
-list1 = [1, 2, 3, 4]
-list2 = [10, 20, 30, 40]
-iterate_lists(list1, list2)
+b_rev = b[::-1]
 
+
+for item_a, item_b in zip(a, b_rev):
+    print(item_a, item_b)
