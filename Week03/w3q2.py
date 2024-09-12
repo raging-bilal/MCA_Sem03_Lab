eabde5c1fa9acbb16ccea1ac167b126596b67f56
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-12 11:32:27 +0530

    Update w3q2.py

--- Week03/w3q2.py
+++ Week03/w3q2.py
@@ -13,10 +13,11 @@
 
 
 
-n = 5
+for i in range(1,6):
+    print("* " * i)
 
-for i in range(1, n + 1):
+
+
+for i in range(4,0,-1):     
     print("* " * i)
     
-for i in range(n - 1, 0, -1):
-    print("* " * i)
