1a95b32e2111703e2f1036313b96893ae04963e6
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-12 12:54:39 +0530

    Update w4q3.py

--- Week04/w4q3.py
+++ Week04/w4q3.py
@@ -1,10 +1,13 @@
 # 3. Given a list of numbers. Write a program to turn every item of a list into its square.
 
-def square_list(lst):
-    return [x ** 2 for x in lst]
+a=[1,2,3,4,5]
+l=len(a)
+
+print(a)
 
 
-numbers = [1, 2, 3, 4, 5]
-squared_numbers = square_list(numbers)
-print(squared_numbers)  
+print("The list of square are as follows: ")
+for i in range(0,l):
+    sq=a[i] * a[i]
 
+    print(sq , end="  ")
