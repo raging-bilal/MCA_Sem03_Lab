7170a22004755d5e9aeb97a223018c15da8280cd
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-12 12:56:45 +0530

    Update w4q5.py

--- Week04/w4q5.py
+++ Week04/w4q5.py
@@ -1,10 +1,8 @@
 # 5. Write a program to count the number of occurrences of item 50 from below tuple tp1:
 # tp1= (50, 10, 60, 70, 50)
 
-def count_occurrences(tp, item):
-    return tp.count(item)
+tp1 = (50, 10, 60, 50, 50, 70, 50)
 
+count_50 = tp1.count(50)
 
-tp1 = (50, 10, 60, 70, 50)
-count_50 = count_occurrences(tp1, 50)
-print(f'Number of occurrences of 50: {count_50}') 
+print("The number of occurrences of 50 is:", count_50)
