badc572cfa97c685055dc55e488de5839890e496
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-12 12:54:16 +0530

    Update w4q2.py

--- Week04/w4q2.py
+++ Week04/w4q2.py
@@ -1,11 +1,16 @@
 # 2. Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
 
 
-def is_first_last_same(lst):
-    if len(lst) < 1:
-        return False  
-    return lst[0] == lst[-1]
+def check(a):
+    if len(a)==0 or a[0]!=a[-1]:
+        return False
+    
+    else:
+        return True
+    
 
+a=[1]
 
-print(is_first_last_same([10, 20, 30, 40, 10]))  
-print(is_first_last_same([10, 20, 30, 40, 50]))  
+r=check(a)
+print("The condition that first and last number of a given list is same is {0} ".format(r))
+    
