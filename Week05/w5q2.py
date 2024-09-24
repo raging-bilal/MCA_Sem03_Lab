720404ac11c888c46835ca9dad4fd5f1494041ac
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-24 19:27:24 +0530

    Update w5q2.py

--- Week05/w5q2.py
+++ Week05/w5q2.py
@@ -1,15 +1,14 @@
 # 2. Write a program to pick a random character from a given String supplied by the user.
 
-import random
+import random as ran
 
+def random_char(my_string):
+    if not my_string:
+        return None
+    else:
+        return ran.choice(my_string)
 
+my_string=input("Enter the String: ")
 
-def pick_random_char(user_string):
-    if not user_string:
-        return None  
-    return random.choice(user_string)
-    
-
-user_string = input("Enter a string: ")
-random_char = pick_random_char(user_string)
-print("Randomly selected character:", random_char)
+result=random_char(my_string)
+print("The randomly generated character: ",result)
