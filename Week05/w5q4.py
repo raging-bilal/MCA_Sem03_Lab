f19e0e6846d833da24cc5142226514f7579f800a
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-25 15:49:19 +0530

    Update w5q4.py

--- Week05/w5q4.py
+++ Week05/w5q4.py
@@ -1,14 +1,26 @@
 # 4. Given a two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
 
 
-# def create_custom_list(list1, list2):
-    
-#     odd_from_list1 = [num for num in list1 if num % 2 != 0]
-#     even_from_list2 = [num for num in list2 if num % 2 == 0]
-#     return odd_from_list1 + even_from_list2
 
-# list1 = [1, 2, 3, 4, 5]
-# list2 = [6, 7, 8, 9, 10]
+n1=int(input("Enter l1 length: "))
+l1=[0]*n1
+for i in range(n1):
+    l1[i]=int(input(">> "))
+print(l1)
 
-# new_list = create_custom_list(list1, list2)
-# print("New List:", new_list)
+n2=int(input("Enter l2 length: "))
+l2=[0]*2
+for i in range(n2):
+    l2[i]=int(input(">> "))
+print(l2)
+
+
+l3=[]
+for i in l1:
+    if i%2!=0:
+        l3.append(i)
+for i in l2:
+    if i%2==0:
+        l3.append(i)
+print("The final list: ")
+print(l3)
