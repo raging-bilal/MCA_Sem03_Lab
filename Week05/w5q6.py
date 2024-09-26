ca65370307e7d2291b299377e09f55a2475ec365
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-26 10:42:28 +0530

    Update w5q6.py

--- Week05/w5q6.py
+++ Week05/w5q6.py
@@ -1,33 +1,29 @@
 # 6. Write a program create a numpy array and sort as per below cases:
-
-# a. Case 1: Sort array by the second row.
-
 import numpy as np
 
-# def sort_by_second_row(arr):
-#     return arr[:, arr[1, :].argsort()]
+arr=np.array(np.random.randint(0,100,(4,5)))
+print("Original array: ")
+print(arr)
 
-# arr = np.array([[34, 12, 25, 45],
-#                 [14, 10, 9, 21],
-#                 [40, 32, 31, 20]])
-
-# sorted_by_second_row = sort_by_second_row(arr)
-# print("Array sorted by second row:\n", sorted_by_second_row)
+# a. Case 1: Sort array by the second row.
+by_row=arr[:,arr[1,:].argsort()]
 
 
-
+print("Sort by Second Row: ")
+print(by_row)
 
 
 
 # b. Case 2: Sort the array by the second column
 
+by_col=arr[arr[:,1].argsort(),:]
 
-def sort_by_second_column(arr):
-    return arr[arr[:, 1].argsort()]
 
-arr = np.array([[34, 12, 25, 45],
-                [14, 10, 9, 21],
-                [40, 32, 31, 20]])
+print("Sort by Second Column: ")
+print(by_col)
 
-sorted_by_second_column = sort_by_second_column(arr)
-print("Array sorted by second column:\n", sorted_by_second_column)
+
+
+
+
+
