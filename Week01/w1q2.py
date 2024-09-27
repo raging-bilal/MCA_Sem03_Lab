0a9b433a989cfa4cfd29bc8c0501b6c403f7b04a
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-27 14:43:37 +0530

    Update w1q2.py

--- Week01/w1q2.py
+++ Week01/w1q2.py
@@ -1,16 +1,12 @@
 # Write a program to print sum of first 10 numbers.
 
 
-
-# i=1
 sum=0
 
-# while i<=10:
 for i in range(1,11):
     sum=sum+i
-    i+=1
     
-print(sum)
+print("Sum of first 10 numbers : ", sum)
 
 
 
