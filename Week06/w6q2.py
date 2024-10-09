4eb67da5290748cb62b1a783a32bfd6a98d0a313
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-10-09 06:30:50 -0400

    Update w6q2.py

--- Week06/w6q2.py
+++ Week06/w6q2.py
@@ -1,22 +1,52 @@
 # 2. Write a Python program to create a Python dictionary (the dictionary type). Add three names and their phone numbers in the dictionary. The names should act as a keys and phones as their values. Then ask the user a name and print its corresponding phone number.
 
-
-def create_phone_book():
-    phone_book = {}
-  
-    for _ in range(3):
-        name = input("Enter name: ")
-        phone_number = input("Enter phone number: ")
-        phone_book[name] = phone_number
+d1={}
 
 
 
-    
-    
-    lookup_name = input("Enter name to look up: ")
-    if lookup_name in phone_book:
-        print(f"The phone number for {lookup_name} is {phone_book[lookup_name]}")
-    else:
-        print(f"{lookup_name} is not in the phone book.")
+for i in range(3):
+    name=input(f"Enter name {i+1}: ")
+    pn=int(input("Enter phone number: "))
+    d1[name]=pn
 
-create_phone_book()
+
+print(d1)
+
+f1=input('Enter name to find phone number: ')
+if f1 in d1:
+    print(f"The phone number of {f1} is {d1[f1]}")
+
+else:
+    print("INVALID INPUT")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
