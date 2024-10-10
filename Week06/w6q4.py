8c8e04068241d38ea00e1ce1c83d68410524d480
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-10-10 02:20:11 -0500

    Update w6q4.py

--- Week06/w6q4.py
+++ Week06/w6q4.py
@@ -1,24 +1,28 @@
 # 4. Write a program that inputs a main string and then creates an encrypted string by embedding a short symbol-based string after each character. The program should also be able to produce the decrypted string from encrypted string.
 
 
-def encrypt_string(main_string, short_string):
-    encrypted = ''
-    for char in main_string:
-        encrypted += char + short_string
-    return encrypted
-
-def decrypt_string(encrypted_string, short_string):
-    decrypted = ''
-    short_string_length = len(short_string)
-    for i in range(0, len(encrypted_string), short_string_length + 1):
-        decrypted += encrypted_string[i]
-    return decrypted
 
 
+def enc(m_s,s_s):
+    e=""
+    for i in m_s:
+        e+=i+s_s
+    return e
 
-main_string = "hello"
-short_string = "!"
-encrypted = encrypt_string(main_string, short_string)
-print("Encrypted String:", encrypted)
-decrypted = decrypt_string(encrypted, short_string)
-print("Decrypted String:", decrypted)
+def dec(e,s_s):
+    d=""
+    s_l=len(s_s)
+    for i in range(0,len(e),s_l +1):
+        d+=e[i]
+        
+    return d
+
+m_s=input("Enter the main string: ")
+s_s=input("Enter the sort symbol: ")
+
+
+print("The ENCRYPTED STRING: ",enc(m_s,s_s))
+print("The DECRYPTED STRING: ",dec(enc(m_s,s_s),s_s))
+
+
+
